93b2833512226e0eb022961a71c451e88afc6bf6
Nikolga-0524-stack <-email@example.com>
2026-06-27 12:37:07 +0300

    Исправлена лишняя скобка в чат-боте

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 const config = {
   name: "Добрыня Никитич",
   slogan: "Забота о вашем питомце с любовью",
-  services: [
-    { name: "Стрижка", price: 2000, duration: "1.5 часа" },
-    { name: "Купание", price: 1500, duration: "1 час" },
-    { name: "Комплекс", price: 3000, duration: "2.5 часа" },
-    { name: "Вычесывание колтунов", price: 1000, duration: "1 час" },
-    { name: "Стрижка когтей", price: 500, duration: "30 минут" }
-  ],
+services: [
+    { name: "Стрижка", duration: "1.5 часа" },
+    { name: "Купание", duration: "1 час" },
+    { name: "Комплекс", duration: "2.5 часа" },
+    { name: "Вычесывание колтунов", duration: "1 час" },
+    { name: "Стрижка когтей", duration: "30 минут" }
+],
   phone: "+7 (963) 680-62-50",
   address: "г. Крсногорск, Московская область ул. Большая Воскресенская, д. 5",
   colors: {
